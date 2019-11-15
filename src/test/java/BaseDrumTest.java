9661e5ddede48c01fd458954ae7ff064d58f304b
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-15 15:26:34 +0000

    Drum fully tested

--- src/test/java/BaseDrumTest.java
+++ src/test/java/BaseDrumTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+import stockItems.instruments.drums.BaseDrum;
+
+import static org.junit.Assert.assertEquals;
+
 public class BaseDrumTest {
+
+    BaseDrum drum;
+
+    @Before
+    public void before(){
+        drum = new BaseDrum();
+    }
+
+    @Test
+    public void canGetDiameter() {
+        assertEquals("22", drum.getDiameter());
+    }
+
+    @Test
+    public void canGetDepth() {
+        assertEquals("18", drum.getDepth());
+    }
 }
