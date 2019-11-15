f34dc7997fb1091f9900b486a1c877368364b949
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-15 15:31:54 +0000

    Strings fully Tested

--- src/test/java/AcousticStringsTest.java
+++ src/test/java/AcousticStringsTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+import stockItems.accessories.strings.AcousticStrings;
+
+import static org.junit.Assert.assertEquals;
+
 public class AcousticStringsTest {
+
+    AcousticStrings strings;
+
+    @Before
+    public void before(){
+        strings = new AcousticStrings();
+    }
+
+    @Test
+    public void canGetWeight() {
+        assertEquals("Light", strings.getWeight());
+    }
+
+    @Test
+    public void canGetWind() {
+        assertEquals("80/20", strings.getWind());
+    }
 }
