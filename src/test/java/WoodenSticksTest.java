60924489df73f3d9b2fc0fae1e0c5d087cf19d15
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-15 16:12:20 +0000

    DrumSticks fully tested

--- src/test/java/WoodenSticksTest.java
+++ src/test/java/WoodenSticksTest.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+import stockItems.accessories.drumsticks.WoodenSticks;
+
+import static org.junit.Assert.assertEquals;
+
 public class WoodenSticksTest {
+
+    WoodenSticks sticks;
+
+    @Before
+    public void before(){
+        sticks = new WoodenSticks();
+    }
+
+    @Test
+    public void canGetWeight() {
+        assertEquals("5", sticks.getWeight());
+    }
+
+    @Test
+    public void canGetDiameter() {
+        assertEquals("A", sticks.getDiameter());
+    }
+
+    @Test
+    public void canGetTipMaterial() {
+        assertEquals("Wood", sticks.getTipMaterial());
+    }
 }
