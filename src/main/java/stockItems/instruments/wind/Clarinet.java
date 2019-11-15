d495bb952e76356a745b91ca2908a14a493eb393
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-15 16:53:54 +0000

    WindInstrument fully tested

--- src/main/java/stockItems/instruments/wind/Clarinet.java
+++ src/main/java/stockItems/instruments/wind/Clarinet.java
@@ -2,6 +2,8 @@ package stockItems.instruments.wind;
 
 public class Clarinet extends WindInstrument {
 
-
+    public Clarinet(){
+        super("LeBlanc", "CL650", "Beginner Clarinet", "Plastic", 150.00, 292.50, "Toot Toot", 17, "Nickel", true);
+    }
 
 }
--- src/test/java/ClarinetTest.java
+++ src/test/java/ClarinetTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+import stockItems.instruments.wind.Clarinet;
+
+import static org.junit.Assert.assertEquals;
+
 public class ClarinetTest {
+
+    Clarinet clarinet;
+
+    @Before
+    public void before(){
+        clarinet = new Clarinet();
+    }
+
+    @Test
+    public void canGetNumberOfValves() {
+        assertEquals(17, clarinet.getNumberOfValves());
+    }
+
+    @Test
+    public void canGetKeyMaterial() {
+        assertEquals("Nickel", clarinet.getKeyMaterial());
+    }
 }
