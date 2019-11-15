c9519ce0215c95d483c415562d596d41219e4dc0
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-15 15:40:10 +0000

    Reeds fully tested

--- src/test/java/SaxReedsTest.java
+++ src/test/java/SaxReedsTest.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+import stockItems.accessories.reeds.SaxReeds;
+
+import static org.junit.Assert.assertEquals;
+
 public class SaxReedsTest {
+
+    SaxReeds reeds;
+
+    @Before
+    public void before(){
+        reeds = new SaxReeds();
+    }
+
+    @Test
+    public void canGetNumberPerBox() {
+        assertEquals(10, reeds.getNumberPerBox());
+    }
+
+    @Test
+    public void canGetStrength() {
+        assertEquals("2.5", reeds.getStrength());
+    }
+
+    @Test
+    public void canSetNumberPerBox() {
+        reeds.setNumberPerBox(5);
+        assertEquals(5, reeds.getNumberPerBox());
+    }
+
+    @Test
+    public void canSetStrength() {
+        reeds.setStrength("3.5");
+        assertEquals("3.5", reeds.getStrength());
+    }
 }
